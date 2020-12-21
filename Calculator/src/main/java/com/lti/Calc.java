de769d521f366c0bc2ec08e2a1264df449e96e14
swasthikNitturi <swasthik305@gmail.com>
2020-12-21 14:46:53 +0530

    commit one

---
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -0,0 +1,15 @@
+package com.lti;
+
+public class Calc {
+
+	public static int add(int a,int b) {
+		System.out.println("addition");
+		return a+b;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(add(10,20));
+
+	}
+
+}
