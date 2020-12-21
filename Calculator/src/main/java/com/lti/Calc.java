d22fd4f65a57ea8cfa2235f3cadf6106b32a67f3
swasthikNitturi <swasthik305@gmail.com>
2020-12-21 14:59:34 +0530

    new

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -3,12 +3,10 @@ package com.lti;
 public class Calc {
 
 	public static int add(int a,int b) {
-	
 		return a+b;
 	}
 	
 	public static int sub(int a,int b) {
-
 		return a-b;
 	}
 	public static void main(String[] args) {
